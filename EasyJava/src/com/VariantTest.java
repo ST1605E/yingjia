99cda0b61537664d02cbba8b05ba85ad884fff8b
Administrator <Administrator@192.168.80.1>
2017-09-05 09:28:20 +0800

    easy

---
+++ EasyJava/src/com/VariantTest.java
@@ -0,0 +1,13 @@
+package com;
+
+public class VariantTest {
+
+	public static int staticVar=0;
+	public int instanceVar=0;
+	public VariantTest(){
+		staticVar++;
+		instanceVar++;
+		System.out.println("staticVar="+staticVar+"---instanceVar"+instanceVar);
+		System.out.println("staticVar="+staticVar+"---instanceVar"+instanceVar);
+	}
+}
